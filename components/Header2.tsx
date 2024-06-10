cfa1c75573ce1ad20f7d47d831f736d06f37f117
north-tower <muneshmyke@gmail.com>
2024-06-10 17:35:12 +0300

    3

--- components/Header2.tsx
+++ components/Header2.tsx
@@ -5,7 +5,7 @@ import { Fragment, useState } from "react";
 import { Bars3Icon , ChatBubbleLeftIcon, HomeIcon, 
     PaperAirplaneIcon,
     ChevronDownIcon, Square3Stack3DIcon,
-    PhoneIcon, PlayCircleIcon,UserGroupIcon,CpuChipIcon,
+    PhoneIcon, PlayCircleIcon,UserGroupIcon,CpuChipIcon,PuzzlePieceIcon,
      XMarkIcon} from "@heroicons/react/24/solid";
 import { Dialog, Disclosure, Popover, Transition } from
  "@headlessui/react"
@@ -46,7 +46,7 @@ function Header2() {
             name: "Integration needs",
             description: "Accounting Software, Payment Gateways, CRM , Bank Integration ",
             href: "#",
-            icon: CpuChipIcon,
+            icon: PuzzlePieceIcon,
         },
        
     ];
@@ -207,18 +207,64 @@ function Header2() {
                         </Popover.Panel>
                     </Transition>
                 </Popover>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                    Flights
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                    Car Rentals
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                    Attractions
-                </a>
-                <a href="#" className="text-sm font-semibold leading-6 text-white">
-                    Flight + Hotel
-                </a>
+                <Popover className="relative">
+                    <Popover.Button className="flex items-center
+                    gap-x-1 text-sm font-semibold leading-6 text-white ">
+                    <span >
+                    Insights and Successes
+                        </span>    
+                        <ChevronDownIcon className="h-5 w-5 flex-none text-white"
+                        aria-hidden="true" />
+                    </Popover.Button>
+
+                    <Transition as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="opacity-0 translate-y-1"
+                    enterTo="opacity-100 translate-y-0"
+                    leave="transition ease-in duration-150"
+                    leaveFrom="opacity-100 translate-y-0"
+                    leaveTo="opacity-0 translate-y-1" >
+
+                        <Popover.Panel 
+                        className="absolute bg-black -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-1 ring-gray-900/5">
+                            <div className="p-4">
+                                {products2.map((item) => (
+                                    <div key={item.name}
+                                    className="group relative flex items-center gap-x-6
+                                    rounded-lg p-4 
+                                    text-sm leading-6 
+                                    hover:bg-sky-50">
+                                       <div className="flex h-11 flex-none items-center justify-center rounded-lg 
+                                       ">
+                                        <item.icon className="h-6 w-6 text-blue-600 group-hover:text-blue-600" 
+                                        aria-hidden="true" />
+                                       
+                                        </div>
+                                        <div className="flex-auto">
+                                            <a href={item.href} 
+                                            className="block 
+                                            font-semibold text-[#013B94] "> {item.name} <span className="absolute inset-0" />
+                                            </a>
+                                            <p className="mt-1 text-[#013B94]">{item.description}</p>
+                                        </div>
+                                        </div>
+                                ))}
+                            </div>
+
+                            {/* <div className="grid grid-cols-2 divide-x divide-gray-900/5 ">
+                                {callsToAction.map((item) => (
+                                    <a key={item.name}
+                                    href={item.href}
+                                    className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013B94] hover:bg-gray-100">
+                                        <item.icon className="h-5 w-5 flex-none text-[#013B94]" aria-hidden="true" />
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div> */}
+                        </Popover.Panel>
+                    </Transition>
+                </Popover>
+                
             </Popover.Group>
 
             <div className="hidden lg:flex lg:flex-1 lg:justify-end">
