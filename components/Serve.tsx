05ce71def96c0ea13c9dbe063dae6091d4c5dc18
north-tower <muneshmyke@gmail.com>
2024-06-10 12:26:33 +0300

    zc

--- components/Serve.tsx
+++ components/Serve.tsx
@@ -7,9 +7,10 @@ function Serve() {
   <div className="mx-auto max-w-screen-xl px-4 md:px-8"> 
     
     <div className="mb-10 md:mb-16">
+      <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Who we serve.</p>
+
       <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Who we serve</h2>
 
-      <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint necessitatibus molestias explicabo.</p>
     </div>
 
     <div className="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
