3821eb78eec51ce3185c5f58a279900337a41ed8
north-tower <muneshmyke@gmail.com>
2024-06-19 16:50:45 +0300

    ggg

--- components/Header2.tsx
+++ components/Header2.tsx
@@ -91,7 +91,7 @@ function Header2() {
     ];
     const callsToAction = [
         { name: "See Demo", href: "#", icon: PlayCircleIcon }, {
-            name: "Contact Support", href: "#", icon: PhoneIcon
+            name: "Contact Support", href: "contact", icon: PhoneIcon
         }
     ]
 
@@ -259,52 +259,17 @@ function Header2() {
                                         </>
                                     )}
                             </Disclosure>
-                            <Disclosure className="-mx-3" as="div" > 
-                                    {( {open} )  => (
-                                        <>
-                                            <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base 
-                                            font-semibold leading-7 text-white hover:bg-blue-800">
-                                               Insights and Successes
-                                                    <ChevronDownIcon className={cn( open ? "rotate-180" : "", "h-5 w-5 flex-none")} aria-hidden="true" />                                               
-                                            </Disclosure.Button>
-                                            <Disclosure.Panel className="mt-2 space-y-2">
-                                                {[...products2].map((item) => (
-                                                    <Disclosure.Button 
-                                                    key={item.name}
-                                                    as="a"
-                                                    href={item.href}
-                                                    className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-800"
-                                                    >
-                                                        {item.name}
-                                                    </Disclosure.Button>
-                                                ))}
-                                            </Disclosure.Panel>
-                                        </>
-                                    )}
-                            </Disclosure>
-                            <Disclosure className="-mx-3" as="div" > 
-                                    {( {open} )  => (
-                                        <>
-                                            <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base 
-                                            font-semibold leading-7 text-white hover:bg-blue-800">
-                                               About Us
-                                                    <ChevronDownIcon className={cn( open ? "rotate-180" : "", "h-5 w-5 flex-none")} aria-hidden="true" />                                               
-                                            </Disclosure.Button>
-                                            <Disclosure.Panel className="mt-2 space-y-2">
-                                                {[...products3].map((item) => (
-                                                    <Disclosure.Button 
-                                                    key={item.name}
-                                                    as="a"
-                                                    href={item.href}
-                                                    className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-white hover:bg-blue-800"
-                                                    >
-                                                        {item.name}
-                                                    </Disclosure.Button>
-                                                ))}
-                                            </Disclosure.Panel>
-                                        </>
-                                    )}
-                            </Disclosure>
+                            <a href="/blog"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+                                Blog</a> 
+                                <a href="/"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+                                About Us</a> 
+                                <a href="/contact"
+                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-blue-800">
+                                Contact Us</a> 
+                            
+                           
                           
                                 
                         </div>
