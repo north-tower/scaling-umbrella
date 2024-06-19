6f859a6f9fed78a85b7b9ae53fdeab1874c16240
north-tower <muneshmyke@gmail.com>
2024-06-19 11:01:51 +0300

    103

---
+++ app/services/page.tsx
@@ -0,0 +1,135 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <section className="w-screen bg-black py-10 text-white">
+  <div className="container mx-auto w-full max-w-screen-xl">
+    <div className="w-full"> 
+      <h2 className="text-center text-3xl font-extrabold">Outsourced bookkeeping services</h2>
+      <p className="mx-auto mb-4 max-w-xl py-2 text-center text-gray-600 sm:text-lg">How does outsourced bookkeeping work.</p>
+    </div>
+    <div  className="bg-black mx-auto py-10 grid max-w-screen-xl grid-cols-1 text-white pl-6 pr-4 sm:px-20 lg:grid-cols-3">
+  <div  className="col-span-1 flex flex-col justify-center text-center sm:text-left md:pr-10">
+    <h1  className="mb-6 text-4xl">How it works..</h1> 
+    <p  className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero.</p>
+  </div>
+  <div  className="col-span-2 mt-10 grid grid-cols-1 gap-6 rounded-2xl bg-gray-600 p-5 sm:p-10 md:grid-cols-2 lg:mt-0">
+    <div  className="relative flex gap-5">
+      <div  className="absolute -left-12 sm:left-auto flex h-10 w-10 items-center justify-center rounded-full bg-gray-800 text-lg text-gray-500 sm:relative md:bg-transparent md:text-5xl">01</div>
+      <div  className=""> 
+        <h3  className="text-xl font-semibold">Initial Consultation and Setup</h3>
+        <p  className="text-gray-400 mt-3">Begin your journey with a comprehensive consultation to understand your unique business needs and financial goals. We'll set up industry-leading bookkeeping software like QuickBooks or Xero and seamlessly migrate your existing financial data into our secure system.</p>
+      </div> 
+    </div>
+    <div  className="relative flex gap-5">
+      <div  className="absolute -left-12 sm:left-auto flex h-10 w-10 items-center justify-center rounded-full bg-gray-800 text-lg text-gray-500 sm:relative md:bg-transparent md:text-5xl">02</div>
+      <div  className="">
+        <h3  className="text-xl font-semibold">Storytelling</h3>
+        <p  className="text-gray-400 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+      </div>
+    </div>
+    <div  className="relative flex gap-5">
+      <div  className="absolute -left-12 sm:left-auto flex h-10 w-10 items-center justify-center rounded-full bg-gray-800 text-lg text-gray-500 sm:relative md:bg-transparent md:text-5xl">03</div>
+      <div  className="">
+        <h3  className="text-xl font-semibold">Keeping the reader hooked</h3>
+        <p  className="text-gray-400 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+      </div>
+    </div>
+    <div  className="relative flex gap-5">
+      <div  className="absolute -left-12 sm:left-auto flex h-10 w-10 items-center justify-center rounded-full bg-gray-800 text-lg text-gray-500 sm:relative md:bg-transparent md:text-5xl">04</div>
+      <div  className="">
+        <h3  className="text-xl font-semibold">Ending with a hook</h3>
+        <p  className="text-gray-400 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<section className="bg-blue-900">
+  <div className="mx-auto grid max-w-lg gap-x-8 gap-y-12 px-4 py-32 md:max-w-screen-xl md:grid-cols-2 md:px-8 lg:grid-cols-3">
+    <div>
+      <h2 className="text-3xl font-medium text-white">
+        Unmatched <br />
+        Services.<br />
+        Unmatched <br />
+        Excellence.
+      </h2>
+    </div>
+
+    <div className="border-white/40 relative border-4 px-4 pt-14 pb-8">
+      <div className="absolute -top-8 left-8 bg-blue-900 px-4 text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-16 w-16">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+        </svg>
+      </div>
+      <p className="mb-3 font-medium uppercase text-white">Integrated Terminal</p>
+      <p className="text-blue-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vero ullam placeat molestiae aspernatur quasi, facere, aliquam ea quia maiores ipsum soluta cumque voluptates.</p>
+    </div>
+    <div className="border-white/40 relative border-4 px-4 pt-14 pb-8">
+      <div className="absolute -top-8 left-8 bg-blue-900 px-4 text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-16 w-16">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+        </svg>
+      </div>
+      <p className="mb-3 font-medium uppercase text-white">Integrated Terminal</p>
+      <p className="text-blue-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vero ullam placeat molestiae aspernatur quasi, facere, aliquam ea quia maiores ipsum soluta cumque voluptates.</p>
+    </div>
+    <div className="border-white/40 relative border-4 px-4 pt-14 pb-8">
+      <div className="absolute -top-8 left-8 bg-blue-900 px-4 text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-16 w-16">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+        </svg>
+      </div>
+      <p className="mb-3 font-medium uppercase text-white">Integrated Terminal</p>
+      <p className="text-blue-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vero ullam placeat molestiae aspernatur quasi, facere, aliquam ea quia maiores ipsum soluta cumque voluptates.</p>
+    </div>
+    <div className="border-white/40 relative border-4 px-4 pt-14 pb-8">
+      <div className="absolute -top-8 left-8 bg-blue-900 px-4 text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-16 w-16">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+        </svg>
+      </div>
+      <p className="mb-3 font-medium uppercase text-white">Integrated Terminal</p>
+      <p className="text-blue-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vero ullam placeat molestiae aspernatur quasi, facere, aliquam ea quia maiores ipsum soluta cumque voluptates.</p>
+    </div>
+    <div className="border-white/40 relative border-4 px-4 pt-14 pb-8">
+      <div className="absolute -top-8 left-8 bg-blue-900 px-4 text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-16 w-16">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
+        </svg>
+      </div>
+      <p className="mb-3 font-medium uppercase text-white">Integrated Terminal</p>
+      <p className="text-blue-200">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vero ullam placeat molestiae aspernatur quasi, facere, aliquam ea quia maiores ipsum soluta cumque voluptates.</p>
+    </div>
+  </div>
+</section>
+
+
+
+<div className="mx-auto my-10 max-w-lg px-4 text-gray-600 md:max-w-screen-lg">
+  <div className="mb-10 flex flex-col border-t-4 border-blue-600 pt-4 md:flex-row">
+    <h2 className="mr-auto mb-4 text-4xl font-medium lg:text-3xl">Experience of <span className="whitespace-nowrap text-blue-600 md:text-gray-600">2 Decades</span></h2>
+    <a href="#" className="text-lg font-medium underline hover:text-blue-600">Read our Story</a>
+  </div>
+  <div className="flex flex-col md:flex-row">
+    <div>
+      <p className="mb-4 md:pr-10 md:text-xl md:leading-relaxed lg:pr-28">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis omnis sint debitis sequi animi quaerat repellendus id distinctio dolores minus.</p>
+      <p className="md:pr-10 md:text-xl md:leading-relaxed lg:pr-28">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus eos quod hic perspiciatis, consectetur suscipit maxime mollitia minima. Enim consequatur ab praesentium ipsum neque dolore expedita, earum rerum reiciendis nihil, deleniti id atque consequuntur necessitatibus distinctio aliquid explicabo quo obcaecati?</p>
+    </div>
+    <p className="hidden uppercase md:block md:text-7xl">
+      Since <br />
+      <span className="whitespace-nowrap text-blue-600">20 Years</span>
+    </p>
+  </div>
+</div>
+
+
+  </div>
+</section>
+
+    </div>
+  )
+}
+
+export default page
