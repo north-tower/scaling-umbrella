3ea50b339ec6e0fe099fed767f860d7c229626c6
north-tower <muneshmyke@gmail.com>
2024-06-20 17:25:16 +0300

    95

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,4 @@
+import Article from '@/components/Article'
 import React from 'react'
 
 function page() {
@@ -14,27 +15,9 @@ function page() {
  
     <div className="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
 
-      <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
-        <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
-          <img src="https://images.unsplash.com/photo-1476362555312-ab9e108a0b7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" loading="lazy" alt="" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-        </a>
-
-        <div className="flex flex-col gap-2">
-          <span className="text-sm text-gray-400">April 2, 2022</span>
-
-          <h2 className="text-xl font-bold text-gray-800">
-            <a href="#" className="transition duration-100 hover:text-rose-500 active:text-rose-600">The Pines and the Mountains</a>
-          </h2>
-
-          <p className="text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint necessitatibus molestias explicabo.</p>
-
-          <div>
-            <a href="#" className="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
-          </div>
-        </div>
-      </article>
+     <Article />
     
-      <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+      {/* <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
         <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
           <img src="https://images.unsplash.com/photo-1511376777868-611b54f68947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" loading="lazy" alt="" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
         </a>
@@ -52,9 +35,9 @@ function page() {
             <a href="#" className="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
           </div>
         </div>
-      </article>
+      </article> */}
       
-      <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+      {/* <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
         <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
           <img src="https://images.unsplash.com/photo-1496395031280-4201b0e022ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" loading="lazy" alt="" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
         </a>
@@ -72,9 +55,9 @@ function page() {
             <a href="#" className="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
           </div>
         </div>
-      </article>
+      </article> */}
  
-      <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
+      {/* <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
         <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
           <img src="https://images.unsplash.com/photo-1510081887155-56fe96846e71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80" loading="lazy" alt="" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
         </a>
@@ -92,7 +75,7 @@ function page() {
             <a href="#" className="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</a>
           </div>
         </div>
-      </article>
+      </article> */}
      
     </div>
   </div>
