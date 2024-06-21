4fbf52301f8efa2f3d2cd5318598e64646f0c59e
north-tower <muneshmyke@gmail.com>
2024-06-21 16:31:25 +0300

    1012

--- components/Article.tsx
+++ components/Article.tsx
@@ -1,25 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Article({id,date, title, description, image}: {id: number, date: string, title: string, description: string, image: string}) {
   return (
     <div>
          <article className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
-        <a href="#" className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
+        <Link href={`/post/${id}`}  className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40">
           <img src={image} />
-        </a>
+        </Link>
 
         <div className="flex flex-col gap-2">
           <span className="text-sm text-gray-400">{date}</span>
 
           <h2 className="text-xl font-bold text-gray-800">
-            <a href="#" className="transition duration-100 hover:text-rose-500 active:text-rose-600">{title}</a>
+          <Link href={`/post/${id}`}>
+
+            <p className="transition duration-100 hover:text-rose-500 active:text-rose-600">{title}</p>
+          </Link>
           </h2>
 
           <p className="text-gray-500">{description}</p>
 
           <div>
+            <Link href={`/post/${id}`}>
+            <p className="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more</p>
             
-            <a href="#" className="font-semibold text-rose-500 transition duration-100 hover:text-rose-600 active:text-rose-700">Read more{id}</a>
+            </Link>
           </div>
         </div>
       </article>
