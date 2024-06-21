0acacf1fe5e01802788eb3eff8308d7b0742ec88
north-tower <muneshmyke@gmail.com>
2024-06-21 16:00:47 +0300

    1007

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -111,8 +111,10 @@ function Page({ params: {postId}}: Props) {
   <div className="mx-auto max-w-screen-xl px-4 md:px-8">
   
     <div className="mb-10 md:mb-16">
-      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Most Recent Posts</h2>
-      <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint necessitatibus molestias explicabo.</p>
+      <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Related Posts</h2>
+      <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">Explore more articles that provide valuable insights and tips on bookkeeping, financial management, and business strategies. These related posts are carefully selected to help you deepen your understanding and enhance your business acumen. Stay informed and discover new ways to improve your business practices.
+
+</p>
     </div>
    
     <div className="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
